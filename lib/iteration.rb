a3fd597ad13ea8646823b3e6fc080e70a3d9d60d
Hannah Drury <hannah_drury@yahoo.com>
2020-01-21 00:38:23 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,12 +6,47 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  
+  newArray = []
+  
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      newArray.push("I love #{src[row_index][element_index]} and #{src[row_index][element_index+1]} on my pizza")
+      element_index += 2
+    end
+    row_index += 1
+    
+  end
+  newArray
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  
+  newArray = []
+  
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      
+      if (src[row_index][element_index]) > (src[row_index][element_index+1])
+        newArray.push(src[row_index][element_index])
+      
+      else
+        newArray.push(src[row_index][element_index+1])
+      end
+    element_index += 2
+    end
+    row_index += 1
+    
+  end
+  newArray
+  
 end
 
 def total_even_pairs(src)
@@ -22,4 +57,23 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+
+  total = 0
+  
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      
+      if (src[row_index][element_index] % 2) == 0 && (src[row_index][element_index+1] % 2) == 0
+        total += (src[row_index][element_index] + src[row_index][element_index+1])
+      end
+
+      element_index += 2
+    end
+    row_index += 1
+    
+  end
+  total
+  
 end
